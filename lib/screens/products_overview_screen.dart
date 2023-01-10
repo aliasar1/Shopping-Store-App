c548dea18f1cc26a9abf25455a493bd20d5b3898
Ali Asar Khowaja <74453775+aliasar1@users.noreply.github.com>
2023-01-10 15:21:35 +0500

    Update products_overview_screen.dart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -68,7 +68,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
-      drawer: AppDrawer(),
+      drawer: const AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
