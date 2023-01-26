790cc81ce668fac23c3872fa787d11e87bec8097
Ali Asar Khowaja <74453775+aliasar1@users.noreply.github.com>
2023-01-26 17:29:07 +0500

    Handled empty list situation.

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/product.dart';
 import '../providers/products.dart';
 import './product_item.dart';
 
@@ -14,19 +13,31 @@ class ProductsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     final products = showFavs ? productsData.favoriteItems : productsData.items;
-    return GridView.builder(
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: products[i],
-        child: ProductItem(product: products[i]),
-      ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-    );
+    return products.isNotEmpty
+        ? GridView.builder(
+            padding: const EdgeInsets.all(10.0),
+            itemCount: products.length,
+            itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: products[i],
+              child: ProductItem(product: products[i]),
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+          )
+        : Center(
+            child: showFavs
+                ? const Text(
+                    "You haven't mark any item as favorite.",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  )
+                : const Text(
+                    "No items available in the store.",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+          );
   }
 }
